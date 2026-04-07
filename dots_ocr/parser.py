82776bf815e7344e89355a300004309151d32f3a
Ahmed Naeem <ahmednaeem.career@gmail.com>
2026-04-07 23:23:27 +0500

    no message

--- dots_ocr/parser.py
+++ dots_ocr/parser.py
@@ -64,7 +64,7 @@ class DotsOCRParser:
         from transformers import AutoModelForCausalLM, AutoProcessor, AutoTokenizer
         from qwen_vl_utils import process_vision_info
 
-        model_path = "./model"
+        model_path = "/model"
         self.model = AutoModelForCausalLM.from_pretrained(
             model_path,
             attn_implementation="flash_attention_2",
